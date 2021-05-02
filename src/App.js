caa71568bd82f2a1b2f634da78b261a3003548c3
akki-96 <75878738+akki-96@users.noreply.github.com>
2021-05-03 01:31:58 +0530

    component-making

--- src/App.js
+++ src/App.js
@@ -3,31 +3,46 @@ import { useState } from "react";
 export default function App() {
   const api = `https://randomuser.me/api`;
   const [user, setUser] = useState([]);
-
   const userHandler = async () => {
     const userData = await fetch(api, { method: "GET" });
     const userJson = await userData.json();
-    // setUser(userJson.results[0].gender);
     const newUser = [...user, userJson.results[0]];
     setUser(newUser);
   };
 
   return (
     <div>
-      <button onClick={userHandler}>Add User</button>
-      <div>
-        {user.map((userObj, index) => {
-          return (
-            <div key={index}>
-              {userObj.name.title} {userObj.name.first} {userObj.name.last}
-              <ol>
-                <li>{userObj.gender}</li>
-                <li>{userObj.email}</li>
-              </ol>
-            </div>
-          );
-        })}
-      </div>
+      <Button userHandler={userHandler} />
+      <UserList user={user} />
     </div>
   );
 }
+
+const Button = ({ userHandler }) => {
+  return <button onClick={userHandler}>Add User</button>;
+};
+
+const UserList = (props) => {
+  const { user } = props;
+  return (
+    <>
+      <div>
+        {user.map((userObj, index) => {
+          return <UserName key={index} userObj={userObj} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+const UserName = ({ userObj }) => {
+  return (
+    <div>
+      {userObj.name.title} {userObj.name.first} {userObj.name.last}
+      <ol>
+        <li>{userObj.gender}</li>
+        <li>{userObj.email}</li>
+      </ol>
+    </div>
+  );
+};
